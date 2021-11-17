ac91b7284d52a5d7998090ed6e866ce73e5cd2ae
nolttr21 <nolttr21@icloud.com>
2021-11-17 08:26:50 -0500

    the buttons now redirect you

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,17 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="en">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Eranthis Communities Home</title>
 
-        
-        
-
-        
+        <link rel="icon" href="/favicon.ico">
     </head>
+
     <body>
-        <button type="button">Login</button>
-        <button type="button">Register</button>
+        <button type="button" onclick="window.location.href='/login'">Login</button>
+        
+        <button type="button" onclick="window.location.href='/register'">Register</button>
     </body>
 </html>
