3d7330c815450bef796dc0738b2be0abec4436c8
Aliza Howe <alizahowe26@gmail.com>
2021-11-19 09:55:10 -0500

    Added redirect banner + changed home banner height

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,10 +9,10 @@
 
 <body>
 
-    <div class="aligner-element"></div>
-    <div class="register-banner"></div>
+    <div class="home-redirect" onclick="location.href='/';"></div> <!-- This makes me a certified HACKER -->
 
     <form action="/api/register" method="POST"> <!-- Add action later! -->
+        <div class="register-banner"></div>
         <p>Select your role:</p>
         <select name="userType" id="userType">        
             <option value="Family Member">Family Member</option>
