6be095394b30dbe7ce84ad720b6536e34a8643fc
nolttr21 <nolttr21@icloud.com>
2021-11-23 07:49:33 -0500

    commit for checkout main

---
+++ resources/views/doctor-home.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" type="text/css" href="{{ asset('logged-in-home.css') }}"/>
+    <title>Doctor Homepage</title>
+    <link rel="icon" href="/favicon.ico">
+</head>
+<body>
+    <div class="home-redirect" onclick="location.href='/';"></div>
+
+    
+</body>
+</html>
