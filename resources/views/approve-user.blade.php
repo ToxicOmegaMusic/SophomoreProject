f92fedca6f094ca2d4e41086beb8822aa67dde61
nolttr21 <nolttr21@icloud.com>
2021-12-06 08:40:32 -0500

    made skeleton of approve-user

--- resources/views/approve-user.blade.php
+++ resources/views/approve-user.blade.php
@@ -7,6 +7,24 @@
 
 <body>
     <table>
-        
+        <tr>
+            <th>Name</th>
+            <th>Role</th>
+        </tr>
+
+        <tr>
+            <td>Wario</td>
+            <td>Admin</td>
+        </tr>
     </table>
+
+    <form>
+        <label for="yes">Yes</label>
+        <input type="radio" name="approve" id="yes">
+
+        <label for="yes">No</label>
+        <input type="radio" name="approve" id="no">
+
+        <input type="submit" name="submit" id="submit" value="next">
+    </form>
 </body>
