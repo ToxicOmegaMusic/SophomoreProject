b9a9939764dbc1c4d2fb99fceced850bf3d6ed13
nolttr21 <nolttr21@icloud.com>
2021-12-09 08:44:31 -0500

    even better now

--- resources/views/approve-user.blade.php
+++ resources/views/approve-user.blade.php
@@ -9,6 +9,7 @@
     <div class="error"></div>
     <table>
         <tr>
+            <th>approve<th>
             <th>Name</th>
             <th>Role</th>
         </tr>
@@ -16,8 +17,7 @@
         <form>
         
         <tr>
-            <label for="approve">Approve</label>
-            <input type="checkbox" name="approve" id="yes">
+            <td><input type="checkbox" name="approve" id="yes">&nbsp;</td>
             <td>Wario</td>
             <td>Admin</td>
         </tr>
