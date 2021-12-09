27e23b27ac9dd9cdcadbfb4e272e5aee793f0596
Aliza Howe <alizahowe26@gmail.com>
2021-12-09 09:43:41 -0500

    Register is done :D

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -9,93 +9,92 @@
 
 <body class="flex flex-col items-center bg-gradient-to-t from-green-900 to-green-400">
 
-    <!-- This is the banner -->
+    <!-- This is the HOME banner -->
     <div class="flex font-serif text-5xl text-center justify-center text-white bg-green-900 h-40 items-center w-full mb-12 cursor-pointer" onclick="location.href='/';">Eranthis<br/>Communities</div>
 
-    <form class="flex flex-col flex-grow bg-green-300 w-3/5 items-center h-auto mb-20" action="http://172.31.109.60:6969/api/register" method="POST">
+    <div class="flex flex-col bg-gradient-to-t from-green-400 to-green-900 rounded-xl w-3/5 mb-20 p-3"> <!-- COOL FORM BORDER -->
+        <form class="flex flex-col bg-green-300 w-full items-center h-auto rounded-xl" action="http://172.31.109.60:6969/api/register" method="POST">
 
-        <!-- The Welcome banner above the form -->
-        <div class="flex flex-col items-center bg-green-100 w-3/5 items-center mt-4 p-4 mb-10">
-            <div class="font-serif text-5xl mb-4 text-green-900">Welcome</div>
-            <div class="font-serif text-lg text-green-900">Register below.</div>
-        </div>
-
-        <p class="flex justify-start text-xl w-80">Select your role</p>
-
-        <select name="userType" id="userType" class="w-80 h-10">        
-            <option value="Family Member">Family Member</option>
-            <option value="Patient" class="ShowExtraFields">Patient</option>
-            <option value="Caregiver">Caregiver</option>
-            <option value="Doctor">Doctor</option>
-            <option value="Supervisor">Supervisor</option>
-            <option value="Admin">Admin</option>
-        </select>
-
-        <div id="normal-register">
-
-            <div class="flex flex-col mt-5 mb-5 w-80">
-                <label for="fName" class="text-xl">First Name</label>
-                <input class="h-10" type="text" name="fName" id="fName" required>
+            <!-- The WELCOME banner above the form -->
+            <div class="flex flex-col items-center bg-green-100 w-3/5 items-center mt-4 p-4 mb-10 rounded-xl">
+                <div class="font-serif text-5xl mb-4 text-green-900">Welcome</div>
+                <div class="font-serif text-lg text-green-900">Register below.</div>
             </div>
 
-            <div class="flex flex-col mb-5">
-                <label for="lName" class="text-xl">Last Name</label>
-                <input class="h-10" type="text" name="lName" id="lName" required>
+            <p class="flex justify-start text-xl w-80">Select your role</p>
+
+            <select name="userType" id="userType" class="w-80 h-10">        
+                <option value="Family Member">Family Member</option>
+                <option value="Patient" class="ShowExtraFields">Patient</option>
+                <option value="Caregiver">Caregiver</option>
+                <option value="Doctor">Doctor</option>
+                <option value="Supervisor">Supervisor</option>
+                <option value="Admin">Admin</option>
+            </select>
+
+            <div id="normal-register">
+
+                <div class="flex flex-col mt-5 mb-5 w-80">
+                    <label for="fName" class="text-xl">First Name</label>
+                    <input class="h-10" type="text" name="fName" id="fName" required>
+                </div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="lName" class="text-xl">Last Name</label>
+                    <input class="h-10" type="text" name="lName" id="lName" required>
+                </div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="email" class="text-xl">Email Address</label>
+                    <input class="h-10" type="email" name="email" id="email" required>
+                </div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="phone" class="text-xl">Phone Number</label>
+                    <input class="h-10" type="text" class="phoneNumber" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" required>
+                </div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="password" class="text-xl">Password</label>
+                    <input class="h-10" type="password" name="password" id="password" required>
+                </div>
+
+                <div class="flex flex-col mb-10">
+                    <label for="DOB" class="text-xl">Date of birth</label>
+                    <input class="h-10" type="date" name="DOB" id="DOB">
+                </div>
+
             </div>
 
-            <div class="flex flex-col mb-5">
-                <label for="email" class="text-xl">Email Address</label>
-                <input class="h-10" type="email" name="email" id="email" required>
+            <div id="isPatientSelected">
+
+                <div class="flex justify-center text-3xl mb-5">Patient Info</div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="FamilyPassword" class="text-xl">Family Password</label>
+                    <input class="h-10" type="password" name="FamilyPassword" id="FamilyPassword">
+                </div>
+
+                <div class="flex flex-col mb-5">
+                    <label for="FamilyContactName" class="text-xl">Emergency Contact Name</label>
+                    <input class="h-10" type="text" name="FamilyContactName" id="FamilyContactName">
+                </div>
+                
+                <div class="flex flex-col mb-5">
+                    <label for="FamilyContactNumber" class="text-xl">Emergency Contact Phone Number</label>
+                    <input class="h-10" type="text" class="phoneNumber" name="FamilyContactNumber" id="FamilyContactNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12">
+                </div>
+                
+                <div class="flex flex-col mb-8">
+                    <label for="FamilyRelation" class="text-xl">Relation to Patient</label>
+                    <input class="h-10" type="text" name="FamilyRelation" id="FamilyRelation">
+                </div>
+
             </div>
 
-            <div class="flex flex-col mb-5">
-                <label for="phone" class="text-xl">Phone Number</label>
-                <input class="h-10" type="text" class="phoneNumber" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12" required>
-            </div>
-
-            <div class="flex flex-col mb-5">
-                <label for="password" class="text-xl">Password</label>
-                <input class="h-10" type="password" name="password" id="password" required>
-            </div>
-
-            <div class="flex flex-col mb-10">
-                <label for="DOB" class="text-xl">Date of birth</label>
-                <input class="h-10" type="date" name="DOB" id="DOB">
-            </div>
-
-        </div>
-
-        <div id="isPatientSelected">
-
-            <!--<div class="divide-green-900"></div>-->
-
-            <div class="flex justify-center text-3xl">Patient Info</div>
-
-            <div class="flex flex-col mb-5">
-                <label for="FamilyPassword" class="text-xl">Family Password</label>
-                <input class="h-10" type="password" name="FamilyPassword" id="FamilyPassword">
-            </div>
-
-            <div class="flex flex-col mb-5">
-                <label for="FamilyContactName" class="text-xl">Emergency Contact Name</label>
-                <input class="h-10" type="text" name="FamilyContactName" id="FamilyContactName">
-            </div>
-            
-            <div class="flex flex-col mb-5">
-                <label for="FamilyContactNumber" class="text-xl">Emergency Contact Phone Number</label>
-                <input class="h-10" type="text" class="phoneNumber" name="FamilyContactNumber" id="FamilyContactNumber" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" maxlength="12">
-            </div>
-            
-            <div class="flex flex-col mb-8">
-                <label for="FamilyRelation" class="text-xl">Relation to Patient</label>
-                <input class="h-10" type="text" name="FamilyRelation" id="FamilyRelation">
-            </div>
-
-        </div>
-
-        <input class="flex justify-center bg-green-900 w-64 font-serif text-3xl text-white rounded-xl h-16 mb-10 hover:bg-green-700" type="submit" name="sub"/>
-    </form>
-
+            <input class="flex justify-center bg-green-900 w-64 font-serif text-3xl text-white rounded-xl h-16 mb-10 hover:bg-green-700" type="submit" name="sub"/>
+        </form>
+    </div>
 
 <!-- Scripts are here -->
 
