521f9002f0012ed1ee060d1ef99022b193989a9d
nolttr21 <nolttr21@icloud.com>
2021-12-10 08:07:09 -0500

    testing 123

---
+++ resources/views/doctor-appointment.blade.php
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<head>
+    <meta charset="utf-8"/>
+    <title>Employee Info</title>
+    <link rel="stylesheet" type="text/css" href="{{ asset('login-reg.css') }}"/>
+</head>
+<body>
